0b4e57e544339027061d0531d72bf224bfe58e80
hausenfefr <hausenfefr@gmail.com>
2017-07-18 02:38:24 -0400

    build to exe

--- mqttagent.py
+++ mqttagent.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2016 Roger Light <roger@atchoo.org>
-#
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Eclipse Distribution License v1.0
-# which accompanies this distribution.
-#
-# The Eclipse Distribution License is available at
-#   http://www.eclipse.org/org/documents/edl-v10.php.
-#
-# Contributors:
-#    Roger Light - initial implementation
-
-# This shows an example of using the subscribe.callback helper function.
-
 import os
 import paho.mqtt.subscribe as subscribe
 
-def print_msg(client, userdata, message):
+def inmsg(client, userdata, message):
 
     if message.topic == "/runner":
+        if message.payload == "stats":
+            print("%s : %s" % (message.topic, message.payload))
+            os.startfile('stats.lnk')
+        if message.payload == "web":
+            print("%s : %s" % (message.topic, message.payload))
+            os.startfile('web.url')
         if message.payload == "calc":
             print("%s : %s" % (message.topic, message.payload))
             os.startfile('calc')
@@ -35,5 +27,5 @@ def print_msg(client, userdata, message):
             os._exit(1)
             
 print("waiting...")
-subscribe.callback(print_msg, "#", hostname="hausenfefr.com")
+subscribe.callback(inmsg, "#", hostname="hausenfefr.com")
 
