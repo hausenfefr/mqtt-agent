24243da966aba6c56e004e8f407c41f069e17114
hausenfefr <hausenfefr@gmail.com>
2017-07-18 02:45:13 -0400

    rebuild

--- mqttagent.py
+++ mqttagent.py
@@ -8,22 +8,16 @@ def inmsg(client, userdata, message):
 
     if message.topic == "/runner":
         if message.payload == "stats":
-            print("%s : %s" % (message.topic, message.payload))
             os.startfile('stats.lnk')
         if message.payload == "web":
-            print("%s : %s" % (message.topic, message.payload))
             os.startfile('web.url')
         if message.payload == "calc":
-            print("%s : %s" % (message.topic, message.payload))
             os.startfile('calc')
         if message.payload == "notepad":
-            print("%s : %s" % (message.topic, message.payload))
             os.startfile('notepad')
         if message.payload == "paint":
-            print("%s : %s" % (message.topic, message.payload))
             os.startfile('mspaint')
         if message.payload == "exit":
-            print("%s : %s" % (message.topic, message.payload))
             os._exit(1)
             
 print("waiting...")
